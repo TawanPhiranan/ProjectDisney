7481f8f03b271d776af9d00da39f1f200bb63099
viewthiti <65011212173@msu.ac.th>
2024-03-03 18:36:38 +0700

    main

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule, Location } from '@angular/common';
-import { ActivatedRoute, Router, RouterModule, RouterOutlet } from '@angular/router';
+import {
+  ActivatedRoute,
+  Router,
+  RouterModule,
+  RouterOutlet,
+} from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
@@ -10,30 +15,42 @@ import { Disney } from '../../model/disney_get_res';
 import { Constants } from '../../config/constants';
 import { HttpClientModule } from '@angular/common/http';
 
-
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [CommonModule, MatToolbarModule, MatIconModule, MatButtonModule, MatCardModule, RouterModule,
-    RouterOutlet, HttpClientModule],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    RouterModule,
+    RouterOutlet,
+    HttpClientModule,
+  ],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.scss'
+  styleUrl: './main.component.scss',
 })
 export class MainComponent implements OnInit {
-
-
   id: any;
   disneys: Disney[] = [];
   user: any;
   img1: any;
   img2: any;
+  imgid1: any;
+  imgid2: any;
+  user2: any;
+  user1: any;
 
-
-  constructor(private route: ActivatedRoute, private location: Location, private http: HttpClient, private constants: Constants, private router: Router) {
-
-  }
+  constructor(
+    private route: ActivatedRoute,
+    private location: Location,
+    private http: HttpClient,
+    private constants: Constants,
+    private router: Router
+  ) {}
   ngOnInit(): void {
-    this.route.queryParams.subscribe(params => {
+    this.route.queryParams.subscribe((params) => {
       this.id = params['id'];
       // console.log(this.id);
       this.callApi();
@@ -53,17 +70,43 @@ export class MainComponent implements OnInit {
     const queryParams = this.user ? { id: this.user.userID } : null;
     this.router.navigate([route], { queryParams });
   }
+  // Choose_route(userNumber: number) {
+  //   const userID = userNumber === 1 ? this.user1.userID : this.user2.userID;
+  //   const route = userID ? '/profile' : '/login';
+  //   const queryParams = userID ? { id: userID } : null;
+  //   this.router.navigate([route], { queryParams });
+  // }
 
   UrlAll() {
     const url = this.constants.API_ENDPOINT + `/profile/image`;
     this.http.get(url).subscribe((data: any) => {
       this.img1 = data[this.randomVote(data)];
+      this.imgid1 = this.img1.imgID;
       do {
         this.img2 = data[this.randomVote(data)];
-      } while (this.img2 === this.img1);
+      } while (this.img2 === this.img1);     
+      this.imgid2 = this.img2.imgID;
+
+      this.userPro();
     });
   }
-  
+
+  userPro(): void {
+    // card 1 
+    const url = this.constants.API_ENDPOINT + `/profile/idm?id=${this.imgid1}`;
+    this.http.get(url).subscribe((data: any) => {
+      this.user1 = data[0] as Disney;
+      // console.log(this.user1);
+    });
+    // card 2
+    const url1 = this.constants.API_ENDPOINT + `/profile/idm?id=${this.imgid2}`;
+    this.http.get(url1).subscribe((data: any) => {
+      this.user2 = data[0] as Disney;
+      // console.log(this.user2);
+      
+    });
+  }
+
   randomVote(array: any[]): number {
     return Math.floor(Math.random() * array.length);
   }
