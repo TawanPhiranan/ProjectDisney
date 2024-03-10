2028e8673ad297cbffb5f5a17638d56617de00d4
TawanPhiranan <65011212119@msu.ac.th>
2024-03-10 22:00:35 +0700

    เเก้อีกละ

--- src/app/config/constants.ts
+++ src/app/config/constants.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
   })
   export class Constants {
     
-    // public readonly API_ENDPOINT: string = 'https://projectdisneyapi-2.onrender.com';
-    public readonly API_ENDPOINT: string =  'http://localhost:3000';
+    public readonly API_ENDPOINT: string = 'https://projectdisneyapi-2.onrender.com';
+    // public readonly API_ENDPOINT: string =  'http://localhost:3000';
 
   }
