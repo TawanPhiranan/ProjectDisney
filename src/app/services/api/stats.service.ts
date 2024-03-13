47eaa56953060f68561801babc263b72520a2559
TawanPhiranan <65011212119@msu.ac.th>
2024-03-13 18:02:53 +0700

    edit 2

--- src/app/services/api/stats.service.ts
+++ src/app/services/api/stats.service.ts
@@ -7,10 +7,26 @@ import { lastValueFrom } from 'rxjs';
   providedIn: 'root'
 })
 export class StatsService {
+  compareRanks() {
+    throw new Error('Method not implemented.');
+  }
   
   constructor(private constants : Constants,private http:HttpClient) { }
 
   public async getAllDailystats(options?:any){
+    let url = '';
+    if(options){
+      const id = options;
+      url = this.constants.API_ENDPOINT + '/rank/yesterday/' + id;
+    }else{
+      url = this.constants.API_ENDPOINT + '/rank/yesterday';
+    }
+    const response = await lastValueFrom(this.http.get(url));
+    return response as any[];
+  }
+
+
+  public async getrankYesterday(options?:any){
     let url = '';
     if(options){
       const id = options;
